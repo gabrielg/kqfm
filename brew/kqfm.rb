1069320349f209d3f3135fcd1371b0cd28f885b3
Gabriel Gironda <gabriel@gironda.org>
2012-05-12 21:40:14 -0500

    Fixes homebrew formula hash mismatch

--- brew/kqfm.rb
+++ brew/kqfm.rb
@@ -5,7 +5,7 @@ require 'formula'
 class Kqfm < Formula
   homepage 'https://github.com/gabrielg/kqfm'
   url 'https://github.com/gabrielg/kqfm/tarball/v1.0.1'
-  md5 '43d8dd9594f08962b4b69689ad3e5f65'
+  md5 '042750478912bc11cc49323fa582af01'
 
   def install
     man1.mkpath
